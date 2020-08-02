09a413d389914768483bd84e90d6a8a04082a7ad
Geulmaster <eyal.geulayev@gmail.com>
2020-08-02 18:01:40 +0300

    Add Overlord's ssh connection class

--- Installer/servers_login.py
+++ Installer/servers_login.py
@@ -1,6 +1,11 @@
-from pexpect import pxssh
+import configparser
+#from pexpect import pxssh
+from Overlord import ssh
 
 class Connect():
+    """
+    Legacy
+    """
     def __init__(self, hostname, username, password):
         self.hostname = hostname
         self.username = username
@@ -17,3 +22,21 @@ class Connect():
 
 #connection = Connect('host', 'user', 'passwd')
 #connection.connect_to_target()
+def config_reader():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config
+
+configuration = config_reader()
+server = configuration['CREDENTIALS']
+
+class Connection():
+    def __init__(self):
+        self.hostname = server['Hostname']
+        self.username = server['Username']
+        self.password = server['Password']
+        print("Trying to login to {}, with the user {}".format(hostname, username))
+
+    def connect(self):
+        connection = ssh.Host(self.hostname, self.username, self.password)
+        connection.connect()
