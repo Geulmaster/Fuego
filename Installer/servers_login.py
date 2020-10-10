8a036084f8259fd04897a10ed100c6ce5ea0a132
Geulmaster <eyal.geulayev@gmail.com>
2020-10-10 19:08:10 +0300

    Add execute function to servers_login module

--- Installer/servers_login.py
+++ Installer/servers_login.py
@@ -10,14 +10,22 @@ def config_reader():
 
 configuration = config_reader()
 server = configuration['CREDENTIALS']
+commands = configuration['COMMANDS']
 
 class Connection():
     def __init__(self):
         self.hostname = server['Hostname']
         self.username = server['Username']
         self.password = server['Password']
-        print("Trying to login to {}, with the user {}".format(hostname, username))
+        print("Trying to login to {}, with user {}".format(self.hostname, self.username))
 
     def connect(self):
         connection = ssh.Host(self.hostname, self.username, self.password)
         connection.connect()
+
+    def execute(self):
+        connection = ssh.Host(self.hostname, self.username, self.password)
+        connection.execute(commands['Execute'], PRINT = True)
+
+con = Connection()
+con.execute()
